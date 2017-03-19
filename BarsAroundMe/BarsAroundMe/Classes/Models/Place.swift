f450b8c1a683446f6e1e46c3b6797f596eb7e077
Giorgos Ampavis <abavisg@gmail.com>
2017-03-19 13:42:34 +0000

    Added Place model object

---
+++ BarsAroundMe/BarsAroundMe/Classes/Models/Place.swift
@@ -0,0 +1,18 @@
+//
+//  Place.swift
+//
+//  Created by Giorgos Ampavis on 19/03/2017
+//  Copyright (c) . All rights reserved.
+//
+
+import Foundation
+
+public struct Place {
+
+    // MARK: Properties
+    public var name: String?
+    public var id: String?
+    public var placeID: String?
+    public var latitude: Float?
+    public var longitude: Float?
+}
