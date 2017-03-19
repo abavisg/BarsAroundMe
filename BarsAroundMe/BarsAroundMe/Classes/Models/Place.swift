2fbfda2731872a967c2707df82b026cc3620b7ee
Giorgos Ampavis <abavisg@gmail.com>
2017-03-19 14:55:52 +0000

    Added code to pass Place model test

--- BarsAroundMe/BarsAroundMe/Classes/Models/Place.swift
+++ BarsAroundMe/BarsAroundMe/Classes/Models/Place.swift
@@ -8,12 +8,28 @@
 import Foundation
 
 public struct Place {
+    
+    // MARK: Declaration for string constants to be used to decode and also serialize.
+    private struct SerializationKeys {
+        static let name = "name"
+        static let id = "id"
+        static let placeID = "placeID"
+        static let latitude = "latitude"
+        static let longitude = "longitude"
+    }
 
     // MARK: Properties
     public var name: String?
     public var id: String?
     public var placeID: String?
-    public var latitude: Float?
-    public var longitude: Float?
+    public var latitude: Double?
+    public var longitude: Double?
     
+    public init(withData data: [String:Any]) {
+        self.name = data[SerializationKeys.name] as? String
+        self.id = data[SerializationKeys.id] as? String
+        self.placeID = data[SerializationKeys.placeID] as? String
+        self.latitude = data[SerializationKeys.latitude] as? Double
+        self.longitude = data[SerializationKeys.longitude] as? Double
+    }
 }
