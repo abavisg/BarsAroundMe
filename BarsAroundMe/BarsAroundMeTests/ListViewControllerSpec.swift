490497ca87b86b5c3094121f8ad43a56a5b67931
Giorgos Ampavis <giorgos.ampavis@piksel.com>
2017-03-20 10:38:34 +0000

    Reafctored Place and PlaceModelTests, added tests for ListViewController instatiation, added list row select action binding with RxSwift, Added google maps handling when tapping list

---
+++ BarsAroundMe/BarsAroundMeTests/ListViewControllerSpec.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewControllerSpec.swift
+//  BarsAroundMe
+//
+//  Created by Giorgos Ampavis on 20/03/2017.
+//  Copyright © 2017 Giorgos Ampavis. All rights reserved.
+//
+
+import Foundation
